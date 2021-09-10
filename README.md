91c34ee4e70dc9f37f99679e1bce74352508e332
Rashud1 <rashudshrestha12@gmail.com>
2021-09-11 09:01:19 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# e-commerce-admin-frontend
+# Ecommerce admin cms
+
+This project is fro the admin panel of the ecommerce website.
+
+## How to run
+
+- Clone the project
+- Run `npm install`
---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      starting admin css
+      
+      
+      </div>
+      
+  );
+}
+
+export default App;
